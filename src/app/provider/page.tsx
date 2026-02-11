6d4499df6feaeddc78fc0495840ef37ddae0b763
liaoyongfeng1020 <liaoyongfeng1020@foxmail.com>
2026-02-11 19:29:54 +0800

    feat: 教员页面

--- src/app/provider/page.tsx
+++ src/app/provider/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import React from 'react';
 
 /**
@@ -9,9 +10,10 @@ export default function DashboardPage() {
   
   return (
     <div className="min-h-screen bg-zinc-50 font-sans">
-      <main className="mx-auto max-w-4xl px-6 py-12">
+      <main className="mx-auto max-w-4xl px-6 py-30">
         <div className="flex-1">
-          <h2 className="mb-2 text-2xl font-semibold text-zinc-900">当前教员为邀请制度，请联系管理员</h2>
+          <h2 className="mb-2 text-2xl font-semibold text-center text-zinc-900">等级达到L8才有资格申请成为教员，您当前的等级为L3</h2>
+          <Link href="/academy" className='flex-1 items-center font-normal'><div className="mt-2 text-lg text-center text-blue-500 hover:text-blue-900">→ 去学习</div></Link>
         </div>
       </main>
     </div>
